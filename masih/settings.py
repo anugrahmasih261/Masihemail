e9c7808f0d6e2e348c7e2b805b4925b6681ab7cf
ANUGRAH MASIH <anugrahmasih1012@gmail.com>
2023-06-01 21:01:54 +0530

    Update settings.py passwords removed

--- masih/settings.py
+++ masih/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT='587'
 EMAIL_USE_TLS='True'
-EMAIL_HOST_USER='anugrahmasih1210@gmail.com'
-EMAIL_HOST_PASSWORD='emotithiybqdfroc'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 
 
 # Activate Django-Heroku.
